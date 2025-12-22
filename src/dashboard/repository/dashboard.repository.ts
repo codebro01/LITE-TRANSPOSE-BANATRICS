1b138c081971bd37f13d2f352d606b540c5c7b2e
codebro <samsondamilola.99@gmail.com>
2025-12-22 11:47:42 +0100

    added my campaigns to business owner dashboard

--- src/dashboard/repository/dashboard.repository.ts
+++ src/dashboard/repository/dashboard.repository.ts
@@ -16,32 +16,46 @@ export class HomeDashboardsRepository {
     @Inject('DB') private DbProvider: NodePgDatabase<typeof import('@src/db')>,
   ) {}
   async businessOwnerHomeDashboard(userId: string) {
-    const [[campaign], [totalSpent], [balance]] = await Promise.all([
-      this.DbProvider.select({
-        totalCampaigns: sql<number>`COUNT(DISTINCT ${campaignTable.id})`,
-        activeCampaigns: sql<number>`COUNT(DISTINCT CASE WHEN ${campaignTable.statusType} = 'active' THEN ${campaignTable.id} END)`,
-        noOfDrivers: sql<number>`SUM(CASE WHEN ${campaignTable.statusType} = 'active' THEN ${campaignTable.noOfDrivers} ELSE 0 END)`,
-      })
-        .from(campaignTable)
-        .where(eq(campaignTable.userId, userId)),
+    const [[campaign], [totalSpent], [balance], [myCampaigns]] =
+      await Promise.all([
+        this.DbProvider.select({
+          totalCampaigns: sql<number>`COUNT(DISTINCT ${campaignTable.id})`,
+          activeCampaigns: sql<number>`COUNT(DISTINCT CASE WHEN ${campaignTable.statusType} = 'active' THEN ${campaignTable.id} END)`,
+          noOfDrivers: sql<number>`SUM(CASE WHEN ${campaignTable.statusType} = 'active' THEN ${campaignTable.noOfDrivers} ELSE 0 END)`,
+        })
+          .from(campaignTable)
+          .where(eq(campaignTable.userId, userId)),
 
-      this.DbProvider.select({
-        totalSpent: sql<number>`SUM(${campaignTable.price})`,
-      })
-        .from(campaignTable)
-        .where(
-          and(
-            eq(campaignTable.userId, userId),
-            eq(campaignTable.paymentStatus, 'spent'),
+        this.DbProvider.select({
+          totalSpent: sql<number>`SUM(${campaignTable.price})`,
+        })
+          .from(campaignTable)
+          .where(
+            and(
+              eq(campaignTable.userId, userId),
+              eq(campaignTable.paymentStatus, 'spent'),
+            ),
           ),
-        ),
 
-      this.DbProvider.select({
-        balance: businessOwnerTable.balance,
-      })
-        .from(businessOwnerTable)
-        .where(eq(businessOwnerTable.userId, userId)),
-    ]);
+        this.DbProvider.select({
+          balance: businessOwnerTable.balance,
+        })
+          .from(businessOwnerTable)
+          .where(eq(businessOwnerTable.userId, userId)),
+
+        this.DbProvider.select({
+          id: campaignTable.id,
+          status: campaignTable.statusType,
+          title: campaignTable.campaignName,
+          state: campaignTable.state,
+          duration: campaignTable.duration,
+          availability: campaignTable.availability,
+          requirements: campaignTable.requirements,
+          description: campaignTable.campaignDescriptions,
+        })
+          .from(campaignTable)
+          .where(eq(campaignTable.userId, userId)),
+      ]);
 
     // console.log({
     //   balance: String(balance.balance),
@@ -57,6 +71,7 @@ export class HomeDashboardsRepository {
       totalCampaign: campaign.totalCampaigns,
       activeCampaign: campaign.activeCampaigns,
       totalNoOfActiveDrivers: campaign.noOfDrivers,
+      myCampaigns: myCampaigns,
     };
   }
   async driverHomeDashboard(userId: string) {
